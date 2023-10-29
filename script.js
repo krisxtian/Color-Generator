956d42985df0d7717180e82671dcd84353b730c2
krisxtian <122443338+krisxtian@users.noreply.github.com>
2023-10-29 01:17:57 -0700

    Random Color Generator
    Automatically generate random color on the DOM when the generate button is click

---
+++ script.js
@@ -0,0 +1,14 @@
+const hex = document.querySelector('.hex');
+const btn = document.querySelector('.generate');
+//const hex = document.querySelector('.hex')
+
+const generateColor = function() {
+    const randomColor = Math.random().toString(13).substring(2,8);
+    document.body.style.backgroundColor = "#" + randomColor;
+    hex.innerHTML = "#" + randomColor;
+}
+ btn.addEventListener('click', generateColor); 
+
+// let color = Math.random();
+// color = Math.random().toString(16).substring(2,8);
+// console.log(color);
